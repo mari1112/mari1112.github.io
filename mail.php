f8c1192c6672dfe5a964518104257d4271312967
mari1112 <39315257+mari1112@users.noreply.github.com>
2018-05-15 22:16:17 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,30 +1,33 @@
-<meta charset="utf-8">
-<?php 
-$message = "Имя: {$_POST['name']}<br>"; // добавляем имя в текст
-$message .= "Email: {$_POST['email']}<br>";
-$message .= "Сообщение: {$_POST['comment']}<br>";
-
-require 'PHPMailer/PHPMailerAutoload.php';
-
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = 'smtp.mail.ru';
-$mail->SMTPAuth = true;
-$mail->Username = 'divina_marinka';
-$mail->Password = '111296061196m';
-$mail->SMTPSecure = 'ssl';
-$mail->Port = '465';
-$mail->CharSet = 'UTF-8';
-$mail->From = 'divina_marinka@mail.ru';
-$mail->FromName = 'Сообщение на сайте';
-$mail->addAddress('');
-$mail->isHTML(true);
-$mail->Subject = 'Новое сообщение с сайта';
-$mail->Body = $message;
-
-if( $mail->send() ){
-echo '<h1 style="color: green; text-align: center;">Ваша заявка принята. Наш менеджер свяжется с Вами в ближайшее время!</h1>';
-}else{
-echo '<p style="color: red;">Ошибка!</p>';
+<meta charset="utf-8"> 
+<?php
+$urok="Урок 22";
+error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+// создание переменных из полей формы		
+if (isset($_POST['name']))			{$name			= $_POST['name];		if ($name == '')	{unset($name);}}
+if (isset($_POST['email']))		{$email		= $_POST['email'];		if ($email == '')	{unset($email);}}
+if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
+//стирание треугольных скобок из полей формы
+if (isset($name) ) {
+$name=stripslashes($name);
+$name=htmlspecialchars($name);
 }
- ?>
+if (isset($email) ) {
+$email=stripslashes($email);
+$email=htmlspecialchars($email);
+}
+if (isset($text) ) {
+$text=stripslashes($text);
+$text=htmlspecialchars($text);
+}
+// адрес почты куда придет письмо
+$address="divina_marinka@mail.com";
+// текст письма 
+$note_text="Тема : $urok \r\nИмя : $name \r\n Email : $email \r\n Дополнительная информация : $text";
+
+if (isset($name)  &&  isset ($sab) ) {
+mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
+// сообщение после отправки формы
+echo "<p style='color:#009900;'>Уважаемый(ая) <b>$name1</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b> $email1</b>.</p>";
+}
+
+?>
