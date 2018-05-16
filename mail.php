7ad65cff156f26de806b3baf1b3ffefd28904413
mari1112 <39315257+mari1112@users.noreply.github.com>
2018-05-16 18:02:02 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,33 +1,40 @@
-<meta charset="utf-8"> 
-<?php
-$urok="Урок 22";
-error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
-// создание переменных из полей формы		
-if (isset($_POST['name']))			{$name			= $_POST['name];		if ($name == '')	{unset($name);}}
-if (isset($_POST['email']))		{$email		= $_POST['email'];		if ($email == '')	{unset($email);}}
-if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
-//стирание треугольных скобок из полей формы
-if (isset($name) ) {
-$name=stripslashes($name);
-$name=htmlspecialchars($name);
-}
-if (isset($email) ) {
-$email=stripslashes($email);
-$email=htmlspecialchars($email);
-}
-if (isset($text) ) {
-$text=stripslashes($text);
-$text=htmlspecialchars($text);
-}
-// адрес почты куда придет письмо
-$address="divina_marinka@mail.com";
-// текст письма 
-$note_text="Тема : $urok \r\nИмя : $name \r\n Email : $email \r\n Дополнительная информация : $text";
+<?php 
 
-if (isset($name)  &&  isset ($sab) ) {
-mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
-// сообщение после отправки формы
-echo "<p style='color:#009900;'>Уважаемый(ая) <b>$name1</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b> $email1</b>.</p>";
-}
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
 
+$name = $_POST['name'];
+$phone = $_POST['email'];
+$email = $_POST['text'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'divina_marinka@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '111296061196m'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('divina_marinka@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('mar_re96@mail.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$email. '<br>Почта этого пользователя: ' .$text;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
 ?>
